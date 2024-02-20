d25783af9c817b09c8344ee7eb7a7fd140adfacc
Takamatsu Naoki <Takamatsu0331@outlook.jp>
2024-02-21 04:37:11 +0900

    :tada: Initial Commit

---
+++ configuration.nix
@@ -0,0 +1,96 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking = {
+    hostName = "TallOaks";
+    networkmanager.enable = true; 
+  };
+
+  powerManagement.enable = true;
+  services.tlp.enable = true;
+  
+  time.timeZone = "Asia/Tokyo";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us"; 
+  };
+
+  services.keyd = {
+    enable = true;
+    keyboards.default = {
+      ids = ["*"];
+      settings = {
+        main = {
+          muhenkan = "oneshot(control)";
+          henkan = "oneshot(shift)";
+          katakanahiragana = "backspace";
+          leftalt = "oneshot(alt)";
+          rightalt = "oneshot(altgr)";
+          meta = "oneshot(meta)";
+          rightcontrol = "oneshot(meta)";
+          capslock = "esc";
+          esc = "capslock";
+          leftshift = "enter";
+        };
+      };
+    };
+  };
+
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+  };
+
+  programs.fish.enable = true;
+
+  users.users.naoki = {
+    isNormalUser = true;
+    extraGroups = ["wheel"];
+    shell = pkgs.fish;
+  };
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  security.polkit.enable = true;
+  hardware.opengl.enable = true;
+  services.xserver.desktopManager.runXdgAutostartIfNone = true;
+
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts-cjk-serif
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      nerdfonts
+      powerline-fonts
+    ];
+    fontDir.enable = true;
+    fontconfig = {
+      defaultFonts = {
+        serif = ["Noto Serif CJK JP" "Noto Color Emoji"];
+      	sansSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
+      	monospace = ["JetBrainsMono Nerd Font"  "Noto Color Emoji"];
+      	emoji = ["Noto Color Emoji"];
+      };
+    };
+  };
+
+  system.stateVersion = "23.11";
+}
+
---
+++ home.nix
@@ -0,0 +1,182 @@
+{ pkgs, lib, ... }: {
+  home.username = "naoki";
+  home.homeDirectory = "/home/naoki";
+  home.stateVersion = "24.05";
+
+  home.packages = with pkgs; [
+    wl-clipboard
+    gnome.adwaita-icon-theme
+    pamixer
+    brightnessctl
+    gimp
+    /*blender
+    globalprotect-openconnect
+    feh*/
+    nil
+    lua-language-server
+    nodePackages_latest.typescript-language-server
+  ];
+
+  programs = {
+    git = {
+      enable = true;
+      userName = "Takamatsu Naoki";
+      userEmail = "Takamatsu0331@outlook.jp";
+    };
+
+    gh = {
+      enable = true;
+    };
+
+    eza = {
+      enable = true;
+      enableAliases = true;
+      git = true;
+      icons = true;
+    };
+
+    fzf = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting
+      '';
+      plugins = [
+        { name = "bobthefisher"; src = pkgs.fishPlugins.bobthefisher.src; }
+        { name = "z"; src = pkgs.fishPlugins.z.src; }
+        { name = "fzf"; src = pkgs.fishPlugins.fzf.src; }
+        {
+          name = "dracula";
+          src = pkgs.fetchFromGitHub {
+            owner = "dracula";
+            repo = "fish";
+            rev = "269cd7d76d5104fdc2721db7b8848f6224bdf554";
+            sha256 = "1xim3yfqf2hmy0gcb58za1cdbgl23bpxqsc815q6qnm6yh8vhahz";
+          };
+        }
+      ];
+    };
+
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      extraLuaConfig = import ./neovim/configuration.nix;
+      plugins = with pkgs.vimPlugins; [
+        oil-nvim
+        kanagawa-nvim
+        nvim-lspconfig
+        nvim-cmp
+        cmp-buffer
+        cmp-path
+        cmp_luasnip
+        cmp-nvim-lsp
+        luasnip
+        friendly-snippets
+      ];
+    };
+
+    waybar = {
+      enable = true;
+      settings = [{
+        layer = "top";
+        position = "top";
+        height = 24;
+        spacing = 4;
+        modules-left = ["sway/scratchpad" "sway/workspaces"];
+        modules-center = ["sway/window"];
+        modules-right = ["pulseaudio" "backlight" "battery" "clock"];
+        "sway/scratchpad" = {
+          format = "[ {count} ]";
+        };
+        pulseaudio = {
+          format-muted = "[{volume}%] 󰝟 ";
+          format = "[{volume}%] {icon} ";
+          format-icons = {
+            default = ["󰕿" "󰖀" "󰕾"];
+          };
+        };
+        backlight = {
+          format = "[{percent}%] {icon} ";
+          format-icons = ["󱩎" "󱩏" "󱩐" "󱩑" "󱩓" "󱩓" "󱩔" "󱩕" "󱩖" "󰛨"];
+        };
+        battery = {
+          format = "[{capacity}%] {icon} ";
+          format-icons = ["󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
+        };
+        clock = {
+          format = "[{:%c}]";
+        };
+      }];
+      style = import ./waybar/style.nix;
+    };
+
+    wofi = {
+      enable = true;
+      settings = {
+        show = "drun";
+        insensitive = true;
+        key_left = "Control_L-h";
+        key_down = "Control_L-j";
+        key_up = "Control_L-k";
+        key_right = "Control_L-l";
+        key_pgup = "Control_L-u";
+        key_pgdn = "Control_L-d";
+        key_expand = "Control_L-l";
+      };
+      style = import ./wofi/style.nix;
+    };
+
+    wezterm = {
+      enable = true;
+    };
+
+    firefox = {
+      enable = true;
+    };
+  };
+
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-mozc ];
+  };
+
+  wayland.windowManager.sway = {
+    enable = true;
+    config = {
+      modifier = "Mod4";
+      terminal = "${pkgs.wezterm}/bin/wezterm";
+      input = {
+        "*" = {
+	        xkb_layout = "us";
+	      };
+        "type:touchpad" = {
+          tap = "enabled";
+          dwt = "enabled";
+	        tap_button_map = "lrm";
+          scroll_method = "two_finger";
+        };
+      };
+      seat = {
+        "*" = {
+          hide_cursor = "3000";
+        };
+      };
+      keybindings = lib.mkOptionDefault {
+        "Mod4+slash" = "exec wofi";
+        "XF86AudioMute" = "exec pamixer -t";
+        "XF86AudioRaiseVolume" = "exec pamixer -i 5";
+        "XF86AudioLowerVolume" = "exec pamixer -d 5";
+        "XF86MonBrightnessUp" = "exec brightnessctl set 10%+";
+        "XF86MonBrightnessDown" = "exec brightnessctl set 10%-";
+      };
+      bars = [{
+        command = "${pkgs.waybar}/bin/waybar";
+      }];
+    };
+  };
+}
