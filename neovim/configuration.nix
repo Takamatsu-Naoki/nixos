d9c51709a2862cf6f9cc7e14a0ad63860c30fadc
Takamatsu Naoki <Takamatsu0331@outlook.jp>
2024-03-15 15:21:20 +0900

    :sparkles: feat: Disable Format on Save

--- neovim/configuration.nix
+++ neovim/configuration.nix
@@ -71,16 +71,6 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettier,
   },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format()
-        end,
-      })
-    end
-  end,
 })
 
 keymap('n', '<leader>f', function() vim.lsp.buf.format { async = true } end)
