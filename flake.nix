dc2f2e41e73f981e236ba2e9a6a43af6273741cc
Takamatsu Naoki <Takamatsu0331@outlook.jp>
2024-07-08 14:23:14 +0900

    :sparkles: feat: Update Flake URL

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-fonts.url = "path:/home/naoki/dev/nixos-fonts";
+    nixos-fonts.url = "github:Takamatsu-Naoki/nixos-fonts";
     nixos-fonts.inputs.nixpkgs.follows = "nixpkgs";
   };
 
