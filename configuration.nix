89cd46e1205da958f95b47fa7f958864be7e1ece
Takamatsu Naoki <Takamatsu0331@outlook.jp>
2024-07-08 14:03:54 +0900

    :bug: fix: Add noto-fonts-color-emoji

--- configuration.nix
+++ configuration.nix
@@ -113,6 +113,7 @@
       noto-fonts
       noto-fonts-cjk-serif
       noto-fonts-cjk-sans
+      noto-fonts-color-emoji
       (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     ] ++ ( with inputs.nixos-fonts.packages.x86_64-linux; [
       anzu-moji
